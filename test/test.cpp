7c4d1e9432f55ad5699e3201fb357af6a93dd9fd
hustlyhang <1959983722@qq.com>
2021-04-19 19:05:02 +0800

    练习

---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+
+
+int main()
+{
+	cout << "64£º" << "sizeof(int) :" << sizeof(int) << endl
+		<< "sizeof(long) :" << sizeof(long) << endl
+		<< "sizeof(long long) :" << sizeof(long long) << endl
+		<< "sizeof(double) :" << sizeof(double) << endl
+		<< "sizeof(char) :" << sizeof(char) << endl;
+}
